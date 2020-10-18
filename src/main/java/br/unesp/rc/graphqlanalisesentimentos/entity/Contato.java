0f10024f3143bd609f385ded8e774e229a8b36f1
LucasAlves17 <lukasmartinsalves17@gmail.com>
2020-10-17 21:22:00 -0300

    adicionado classe contato e login

---
+++ src/main/java/br/unesp/rc/graphqlanalisesentimentos/entity/Contato.java
@@ -0,0 +1,69 @@
+package br.unesp.rc.graphqlanalisesentimentos.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "contato")
+public class Contato implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "celular")
+    private String celular;
+    
+    @OneToOne
+    @JoinColumn(name = "usuario_id",nullable = false, updatable = false)
+    private Usuario usuario;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+}
+    
+    
---
+++ src/main/java/br/unesp/rc/graphqlanalisesentimentos/entity/Login.java
@@ -0,0 +1,67 @@
+package br.unesp.rc.graphqlanalisesentimentos.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "login")
+public class Login implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(name = "nome_usuario")
+    private String nomeUsuario;
+    
+    @Column(name = "senha")
+    private String senha;
+    
+    @OneToOne
+    @JoinColumn(name = "usuario_id",nullable = false, updatable = false)
+    private Usuario usuario;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+}
